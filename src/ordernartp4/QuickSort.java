620a36ea7e0665c29a79407261c59d07b4eb2042
Martin-J-Larre <martinjlarre@gmail.com>
2024-04-27 00:02:33 -0400

    #1 Copiado e indentado/ código revisado para encontrar TODO

---
+++ src/ordernartp4/QuickSort.java
@@ -0,0 +1,46 @@
+//Nombre: Nombre del alumno
+//Legajo: INFXXXXX
+//DNI: XXXXXXXX
+
+package ordernartp4;
+
+
+public class QuickSort {
+    /* Esta función toma el último elemento como pivote,
+    * coloca el elemento pivote en su posición correcta en la matriz
+    ordenada
+    * y coloca todos los elementos más pequeños (más pequeños que pivote)
+    * a la izquierda del pivote y todos los elementos mayores a la derecha
+    del pivote.
+    */
+    int particion(int arr[], int low, int high){
+        int pivot = arr[high];
+        int i = (low-1); // índice del elemento mas chico
+        
+        for (int j=low; j<high; j++){
+        // Si el elemento actual es mas chico o igual que el pivot
+            if (arr[j] <= pivot){
+                i++;
+            // swap arr[i] and arr[j]
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            }
+        }
+        // Intercambia arr[i+1] y arr[high] (o pivot)
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i+1;
+    }
+    
+    /* Método principal que implementa quicksort
+    arr[] --> Array a ser ordenado,
+    low --> Comienzo de indice,
+    high --> Fin de indice */
+    void ordenar(int arr[], int low, int high){
+        // ********   TODO  *********
+        // ********   TODO  *********
+        // ********   TODO  *********
+    }
+}
